6350994383365e44a65a4a50bc15cdad5b5bc72c
PyeonMinjun <alswns9810@gmail.com>
2024-03-09 23:56:45 +0900

    first commit

---
+++ src/main/java/com/example/springcheatsheet/SpringcheatsheetApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcheatsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcheatsheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcheatsheetApplication.class, args);
+	}
+
+}
